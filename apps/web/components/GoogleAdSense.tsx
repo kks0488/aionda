072a0e1d52820b72a3768170387837d5506fe017
kkaemo <kkaemo@example.com>
2026-02-03 15:32:51 +0900

    adsense: add ads.txt and load script afterInteractive

--- apps/web/components/GoogleAdSense.tsx
+++ apps/web/components/GoogleAdSense.tsx
@@ -12,7 +12,7 @@ export default function GoogleAdSense() {
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_ID}`}
       crossOrigin="anonymous"
-      strategy="beforeInteractive"
+      strategy="afterInteractive"
     />
   );
 }
